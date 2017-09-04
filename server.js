227a07f6335a8a886086b9615d9d845c9938d9f1
Lee Bryant <elvie.bryant@gmail.com>
2017-09-04 15:13:30 -0700

    syntax update

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('public'))
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Server started on port: 3000')
 })
