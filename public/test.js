be7c91be1b75603389c93106a8b90e8ad5b41832
Lee Bryant <elvie.bryant@gmail.com>
2017-09-15 19:52:26 -0700

    cleaned up test file.

--- public/test.js
+++ public/test.js
@@ -5,59 +5,33 @@ var data = JSON.stringify({
   }
 });
 
-// var xhr = new XMLHttpRequest();
-// xhr.withCredentials = true;
-
-// xhr.addEventListener("readystatechange", function () {
-//   if (this.readyState === 4) {
-//     console.log(this.responseText);
-//   }
-// });
-
-// xhr.open("POST", "https://conduit.productionready.io/api/users/login");
-// xhr.setRequestHeader("content-type", "application/json");
-// xhr.setRequestHeader("x-requested-with", "XMLHttpRequest");
-// xhr.setRequestHeader("cache-control", "no-cache");
-
-// xhr.send(data);
 console.log(fetch);
 
 fetch('https://conduit.productionready.io/api/tags', {
   method: 'GET',
   headers: {
     'Content-Type': 'application/json'
-  }  
-}).then(function(data){
+  }
+}).then(function (data) {
   return data.json()
 })
-.then(function (result){
-  console.log(result)
-})
-// .catch(function(error) {
-//   console.log('request failed', error)
-// });
+  .then(function (result) {
+    console.log(result)
+  })
 
-let request = { 
-  method: "POST", 
-  headers: {"content-type": "application/json"}, 
-  body: JSON.stringify({user: {email:"",password:""}}),     
+let request = {
+  method: "POST",
+  headers: { "content-type": "application/json" },
+  body: JSON.stringify({ user: { email: "", password: "" } }),
 }
-// fetch("https://conduit.productionready.io/api/users", request)
-//   .then(function(response){
-//     return response.text()
-//   })
-//   .then(function(body){
-//     console.log(body)
-//   });
 
-  
-  console.log(request);
-  var result = fetch("https://conduit.productionready.io/api/users", request)
-    .then((function (prim) {
-          return prim.text();
-        }))
-    .then(function(final){
-      console.log("==================================================================")
-      return Promise.resolve(console.log(final));
-    })
-  console.log(result);
+console.log(request);
+var result = fetch("https://conduit.productionready.io/api/users", request)
+  .then((function (prim) {
+    return prim.text();
+  }))
+  .then(function (final) {
+    console.log("==================================================================")
+    return Promise.resolve(console.log(final));
+  })
+console.log(result);
